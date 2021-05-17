d8df0b96b5cd2063a7f3aaf96fc58a4fcc83b350
clo-kinng <83395366+clo-kinng@users.noreply.github.com>
2021-05-17 13:59:14 +0900

    修正１回目

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,13 +4,15 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to "/prototypes/#{@comment.prototype.id}"
     else
-      @prototype = @comment.prototype
-      @comments = @prototype.comments
-      render "prototypes/show"
+      @prototype = Prototype.find(params[:prototype_id])
+      @comment = Comment.new
+      @comments = @prototype.comments.includes(:user)
+      render "prototypes/show"      
     end
   end
 
   private
+ 
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
