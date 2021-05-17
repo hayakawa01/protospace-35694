817da8fb43e24a7787426e5b475d83c604309085
clo-kinng <83395366+clo-kinng@users.noreply.github.com>
2021-05-17 15:25:19 +0900

    修正３回目

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,11 +6,13 @@ class CommentsController < ApplicationController
     else
       @prototype = Prototype.find(params[:prototype_id])
       @comment = Comment.new
-      render :show      
+      @comments = @prototype.comments.includes(:user)
+      render "prototypes/show"      
     end
   end
 
   private
+ 
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
